5f16968cf9be45ff7ded4b0932f4f52fe7c59959
jordybsk <dylan.bossoku@jobtrek.ch>
2024-01-29 14:53:43 +0100

    we can add a todo

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-$file = 'name.txt';
-$nom ='';
+$todos = [];
 
-if (isset($_POST["submitBtn"]) ){
-
-    $nom = $_POST["inputName"];
-    file_put_contents($file, $nom . "," . "\n", FILE_APPEND);
-    echo file_get_contents($file);
+if (file_exists('todos.json')) {
+    $todos = json_decode(file_get_contents('todos.json'), true);
 }
+
+if (isset($_POST["submitBtn"])) {
+
+    $newTodo = [htmlspecialchars($_POST["todoName"]), $_POST["todoDate"]];
+
+    $todos[] = $newTodo;
+
+    file_put_contents('todos.json', json_encode($todos));
+
+    header("location:index.php");
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -15,15 +24,44 @@ if (isset($_POST["submitBtn"]) ){
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<h1>ton Nom: <?php htmlspecialchars($nom)?></h1>
-<form action="index.php" method="post">
+<?php
+foreach($todos as $todo){
+    echo  $todo[0];
+    echo  $todo[1];
 
-    <input name="inputName" type="text">
-    <button name="submitBtn" type="submit">submit</button>
+}
 
+?>
+
+
+<form class="max-w-md mx-auto mt-12" method="post">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="todoName"
+               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+               placeholder=" " required/>
+        <label for="floating_email"
+               class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">nom
+            de ta tache</label>
+    </div>
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="date" name="todoDate"
+               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+               placeholder=" " required/>
+        <label for="floating_password"
+               class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">date
+            de ta tache</label>
+    </div>
+
+
+    <button type="submit" name="submitBtn"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Submit
+    </button>
 </form>
+
 
 </body>
 </html>
